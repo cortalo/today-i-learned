0eac22328ac0297c822554631a7b365b86cdf551
Cortalo <Cortalo@outlook.com>
2025-10-30 14:57:31 +0100

    ready for production

--- src/App.js
+++ src/App.js
@@ -276,6 +276,8 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     const { data: updatedFact, error } = await supabase
@@ -294,6 +296,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[⛔DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">
           (Source)
